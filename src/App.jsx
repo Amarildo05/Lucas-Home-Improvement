80fce5fc4b7d66c143c4ccc9d6c7999ac54465c9
Amarildo <amarildokola36@gmail.com>
2025-09-22 23:08:16 +0200

    setup tailwindCss

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 function App() {
-
   return (
-    <div>Lucas Home Improvement</div>
-  )
+    <div className="h-screen flex flex-col justify-center items-center bg-gray-400">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Lucas Home Improvement
+      </h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
